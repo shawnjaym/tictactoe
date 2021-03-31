e5ba1d439cbd6e30ae0cfd062dd42938dc718582
shawnjaym <75267704+shawnjaym@users.noreply.github.com>
2021-03-31 12:55:08 -0400

    Add files via upload

---
+++ player.py
@@ -0,0 +1,40 @@
+import math
+import random
+​
+class Player:
+    def ___init___ (self, letter):
+        #letter is x or o
+        self.letter = letter
+​
+        # we want all players to get their next move given a game
+        def get_move(self, game):
+        pass
+​
+class RandomComputerPlayer(player):
+    def ___init___(self, player):
+        super().___init___(letter)
+        
+    def get_move(self, letter):
+        square = random.choice(game.available_moves())
+​
+    class HumanPlayer(Player):
+        def ___init___(self, letter):
+            super().___init___(letter)
+​
+        def get_move(self, game):
+            valid_square = False
+            val = None
+            while not valid_square:
+                square = input(self.letter + '\'s turn. Input move (0-9):')
+                # we're going to check that this is a correct value by trying to cast
+                # it to an integer, and if it's not, then we say its invalid
+                # if that spot is not available on the board, we also say its invalid
+                try:
+                    val = int(square)
+                    if val not in game.available_moves():
+                        raise ValueError:
+                        valid_square = True # if these are successful, then yay !
+                    except ValueError:
+                        print('Invalid square. Try again')
+​
+                    return val
